cfa23613bc214e13c721f76b489bbab9ca15b811
Asante <asanterashid.biz@gmail.com>
2021-01-07 18:28:08 -0500

    added message if no perameters were chosen

--- script.js
+++ script.js
@@ -3,18 +3,28 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  confirm("Do you want uppercase letters included?");
+  // 
+  var UpperCaseYN = confirm("Do you want upper case letters included?");
+  var LowerCaseYN = confirm("Do you want lower case letters included?");
+  var NumberYN = confirm("Do you want numbers included?");
+  var SpecialCharactersYN = confirm("Do you want special characters included?");
+  if (UpperCaseYN == false && LowerCaseYN == false && NumberYN == false && SpecialCharactersYN == false) {
+    alert("Password can't be created")
+  };
+  // if none are selected alert that a password can't be made under these conditions
+  prompt("how many characters do you want in your password? (value must be between 8 and 128)")
+  // 
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
- confirm("do you wasnt upper and lowercase letters?");
- 
-  
+
+
   passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
- 
+
 // based on demo 8 i should have a series of confirms as a yes or no to special criteria
 // ex lowercase
 // ex uppercase
