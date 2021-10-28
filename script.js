25cb96e34cd921dae3093279217c2a4b6d571b54
Asante <asanterashid.biz@gmail.com>
2021-10-27 20:07:36 -0400

    character validation exits the prompt if no options are selected

--- script.js
+++ script.js
@@ -11,57 +11,57 @@ var GeneratedPassword = "";
 
 // Write password to the #password input
 function writePassword() {
-  
+  characterInquiry();
+}
+
+function characterInquiry() {
+  // character inquirer prompts
   var UpperCaseYN = confirm("Do you want upper case letters included?");
   if (UpperCaseYN) {
     CharBank.push.apply(CharBank, UpperCaseCharacters);
-    console.log(CharBank);
   }
   var LowerCaseYN = confirm("Do you want lower case letters included?");
   if (LowerCaseYN) {
     CharBank.push.apply(CharBank, LowerCaseCharacters);
-    console.log(CharBank)
   }
   var NumberYN = confirm("Do you want numbers included?");
   if (NumberYN) {
     CharBank.push.apply(CharBank, NumericCharacters);
-    console.log(CharBank)
   }
   var SpecialCharactersYN = confirm("Do you want special characters included?");
   if (SpecialCharactersYN) {
     CharBank.push.apply(CharBank, SpecialCharacters);
-    console.log(CharBank)
   }
 
   //! if none are selected alert that a password can't be made under these conditions
-  // if (UpperCaseYN == false && LowerCaseYN == false && NumberYN == false && SpecialCharactersYN == false) {
-  //   alert("Password can't be created")
-  // } else {
-  //   // need to add a alert here to ask user to enter appropriate number in one isn't chosen
-  //   console.log(PasswordLength)
-  //   console.log(parseInt(PasswordLength));
-  // }
+  if (UpperCaseYN == false && LowerCaseYN == false && NumberYN == false && SpecialCharactersYN == false) {
+    alert("Password can't be created");
+    end();
+  } else {
+    generatePassword();
+  }
+};
 
+function generatePassword() {
+  // passwordgenerator function using available character bank
+  var PasswordLength = prompt("how many characters do you want in your password? (value must be between 8 and 128)");
+  //! if selected number isn't between 8-128 then a prompt pops up warning the user and makes them enter an acceptable number
 
   // loop that takes the available characters and concatinates them until the chosen password length is reached
-
-  var PasswordLength = prompt("how many characters do you want in your password? (value must be between 8 and 128)");
-  
   for (var i = 0; i < PasswordLength; i++) {
-    
+
     var num = Math.floor(Math.random() * CharBank.length) + 1;
     PasswordCharacter = CharBank[num];
     GeneratedPassword += PasswordCharacter;
-    console.log(GeneratedPassword);
   };
 
-  // var password = generatePassword(); // why is this line here
-
   passwordText.value = GeneratedPassword;
-}
+};
 
-// Add event listener to generate button
+function end() {
+  return;
+};
+//  event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-// this was given
 
 
